f52f8c13addeb199b34719406521ef68c6b8d212
Luka Nićiforović <lukanici@gmail.com>
2019-01-24 02:19:48 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ function ispisiIme() {
 
 ispisiIme();
 setInterval(ispisiIme, 300);
+
+var meni = document.getElementById('meni');
+var prikazan = false;
+function prikaziMeni() {
+    if (prikazan) {
+        meni.style.display = "none";
+        prikazan = false;
+    }
+    else {
+        meni.style.display = "block";
+        prikazan = true;
+    }
+}
+
+window.onresize = function(e) {
+    if (window.innerWidth > 616)
+        meni.style.display = "block";
+    else {
+        if (prikazan)
+            meni.style.display = "block";
+        else
+            meni.style.display = "none";
+    }
+}
+
+var idiGore = document.getElementById('idiGore');
+window.onscroll = function(e) {
+    if (window.scrollY > 50)
+        idiGore.style.display = "block";
+    else
+        idiGore.style.display = "none";
+}
